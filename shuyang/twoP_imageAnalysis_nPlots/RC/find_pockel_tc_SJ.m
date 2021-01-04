7db17bcc4ad33a1a8b1ec6594a9082a98f617de9
Sjin22 <35850502+Sjin22@users.noreply.github.com>
2021-01-04 13:47:11 -0500

    new analysis code

--- shuyang/twoP_imageAnalysis_nPlots/RC/find_pockel_tc_SJ.m
+++ shuyang/twoP_imageAnalysis_nPlots/RC/find_pockel_tc_SJ.m
@@ -1,7 +1,7 @@
 % function designed to determine that state of the pockel cell by using the changes in whole field fluorescence
 function pockel_tc = find_pockel_tc(data, tc_switch)
 %input - data - should be the full 3D matrix of the 2P imaging data
-    %tc_switch - should =0 for full data movie and 1 for just the TCs  size = frames,neurons
+%tc_switch - should =0 for full data movie and 1 for just the TCs  size = frames,neurons
 %output - pockel_tc - a boolean timecourse where frames with laser power =1    and frames where pockel cell 
 % reduces laser power = 0
 
@@ -12,12 +12,12 @@ else
     if numel(size(data)) > 3
         data=squeeze(data);
     end
-    data_grand_avg = squeeze(mean(mean(data,2),1))';
+    data_grand_avg = squeeze(mean(mean(data,2),1))'; % average across vertical and horizontal pixels, each frame is a number
 end
 
 %calculate some useful variables
 data_round = round(data_grand_avg);
-data_mode = mode(data_round(1:round(length(data_grand_avg)/3)));
+data_mode = mode(data_round(1:round(length(data_grand_avg)/3))); % most frequent values in data_avg
 data_deriv = diff(data_grand_avg);
 data_deriv_std = std(data_deriv);
 
@@ -39,9 +39,9 @@ end
 
 for laser_trans_ind = laser_on
     if laser_off(end) > laser_trans_ind
-        pockel_tc(1,laser_trans_ind+2:laser_off(find(laser_off>laser_trans_ind, 1, 'first'))-2) = 1; %remove in two frames of buffer on either side
+        pockel_tc(1,laser_trans_ind+3:laser_off(find(laser_off>laser_trans_ind, 1, 'first'))-3) = 1; %remove in three frames of buffer on either side
     else
-        pockel_tc(1,laser_trans_ind+2:end) = 1;
+        pockel_tc(1,laser_trans_ind+3:end) = 1;
     end
 end
 %pockel_tc(1) = 0% added this because first frame is a transition, satisfies next assertion
