822fe0e3cf65abc38963bc08c2c4c5d16dce4312
Lindsey <llglick@gmail.com>
2021-08-18 12:02:25 -0400

    deal with discontinuity and add stimOffFrames to frameFinder

--- lindsey/SBXscripts/photoFrameFinder_Sanworks.m
+++ lindsey/SBXscripts/photoFrameFinder_Sanworks.m
@@ -1,11 +1,20 @@
-function [stimOnFrames] = photoFrameFinder_Sanworks(events);
+function [stimOnFrames stimOffFrames] = photoFrameFinder_Sanworks(events);
 %new code for Sanworks photodiode
 %events is info.frames from scanbox .mat file
+    %fix discontinuity
+    if find(diff(events)<0)
+        events(find(diff(events)<0)+1:end) = events(find(diff(events)<0)+1:end)+2^16;
+    end
+        %remove any 0s from frame list
+    if find(events == 0)
+        events(find(events==0)) = [];
+    end
     framesOn = unique(events);
     nframes = max(framesOn(:));
     frameMat = zeros(1,nframes);
     frameMat(framesOn) = 1;
     frameTrig = diff(frameMat);
     stimOnFrames = find(frameTrig == 1);
+    stimOffFrames = find(frameTrig == -1);
 end
     
