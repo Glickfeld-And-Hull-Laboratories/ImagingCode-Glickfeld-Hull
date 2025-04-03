86083d3f2bec16cf9163284ac4dea29588a7822f
LindseyG <llglick@gmail.com>
2025-04-03 15:42:34 -0400

    Colors- LG

--- lindsey/greens.m
+++ lindsey/greens.m
@@ -1,14 +1,14 @@
-function x = blues(num,scale);
+function x = greens(num,scale);
 % create a color map of blue: pale to dark
 x = zeros(num,3);
-x(:,3) = 1;
+x(:,2) = 1;
 if nargin<2
     scale = 'linear';
 end
 if strcmp(scale,'log')
     x(:,1) = 1-logspace(log(1/num),log(1-(1/num)),num);
-    x(:,2) = 1-logspace(log(1/num),log(1-(1/num)),num);
+    x(:,3) = 1-logspace(log(1/num),log(1-(1/num)),num);
 elseif strcmp(scale,'linear')
-    x(:,1) = 1-(1/num):-1/num:0;
-    x(:,2) = 1-(1/num):-1/num:0;
+    x(:,1) = 1-(1/num):1/num:0;
+    x(:,3) = 1-(1/num):1/num:0;
 end
