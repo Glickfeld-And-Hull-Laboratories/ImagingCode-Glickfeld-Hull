f6d873069cab4599d8955478b40d7b704b8ecc7c
ArdentRodent <tingweijerryhu@gmail.com>
2025-10-29 15:18:35 -0400

    DART pipeline step 1 update

--- jerry/instr_Jerry.m
+++ jerry/instr_Jerry.m
@@ -1,4 +1,3 @@
 %% Instruction sheet
-instructions.session='44';
-instructions.ds='DART_expt_info_jerry';
-instructions.refDay='2'; % 1 if baseline, 2 if post-drug
+instructions.session='38';
+instructions.ds='DART_V1_YM90K_Celine';
