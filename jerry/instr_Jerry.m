114236c6a9ae7043dd0de7b6069b7345ade22ee9
ArdentRodent <tingweijerryhu@gmail.com>
2026-01-13 17:52:32 -0500

    regular pull

--- jerry/instr_Jerry.m
+++ jerry/instr_Jerry.m
@@ -1,7 +1,7 @@
 %% Instruction sheet
 instructions.session='2';
 instructions.ds='DREADD_V1_datasheet';
-instructions.tIdxSource = ["PD" "PD"]; % PD or MW or cS; ref day then match day
+instructions.tIdxSource = ["PD"]; % PD or MW or cS; ref day then match day
 instructions.refDay='2'; % 1st day or 2nd day used as reference
 instructions.tDropBool = false; % true or false
 instructions.tDropRefDay = []; % ref day is always instructions.session
